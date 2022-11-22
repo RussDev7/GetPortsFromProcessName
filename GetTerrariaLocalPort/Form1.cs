37b0ae16fb59eff7d64c8a149f6066735cbb9b63
DannyRuss <33048298+RussDev7@users.noreply.github.com>
2022-11-21 16:35:28 -0800

    Converted the code to a class.

--- GetTerrariaLocalPort/Form1.cs
+++ GetTerrariaLocalPort/Form1.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
-using System.Linq;
-using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace GetTerrariaLocalPort
@@ -13,153 +10,16 @@ namespace GetTerrariaLocalPort
             InitializeComponent();
         }
 
-        public enum TCP_TABLE_CLASS : int
-        {
-            TCP_TABLE_BASIC_LISTENER,
-            TCP_TABLE_BASIC_CONNECTIONS,
-            TCP_TABLE_BASIC_ALL,
-            TCP_TABLE_OWNER_PID_LISTENER,
-            TCP_TABLE_OWNER_PID_CONNECTIONS,
-            TCP_TABLE_OWNER_PID_ALL,
-            TCP_TABLE_OWNER_MODULE_LISTENER,
-            TCP_TABLE_OWNER_MODULE_CONNECTIONS,
-            TCP_TABLE_OWNER_MODULE_ALL
-        }
-
-        [StructLayout(LayoutKind.Sequential)]
-        public struct MIB_TCPROW_OWNER_PID
-        {
-            public uint state;
-            public uint localAddr;
-            public byte localPort1;
-            public byte localPort2;
-            public byte localPort3;
-            public byte localPort4;
-            public uint remoteAddr;
-            public byte remotePort1;
-            public byte remotePort2;
-            public byte remotePort3;
-            public byte remotePort4;
-            public int owningPid;
-
-            public ushort LocalPort
-            {
-                get
-                {
-                    return BitConverter.ToUInt16(
-                        new byte[2] { localPort2, localPort1 }, 0);
-                }
-            }
-
-            public ushort RemotePort
-            {
-                get
-                {
-                    return BitConverter.ToUInt16(
-                        new byte[2] { remotePort2, remotePort1 }, 0);
-                }
-            }
-        }
-
-        [StructLayout(LayoutKind.Sequential)]
-        public struct MIB_TCPTABLE_OWNER_PID
-        {
-            public uint dwNumEntries;
-            MIB_TCPROW_OWNER_PID table;
-        }
-
-        [DllImport("iphlpapi.dll", SetLastError = true)]
-        static extern uint GetExtendedTcpTable(IntPtr pTcpTable,
-            ref int dwOutBufLen,
-            bool sort,
-            int ipVersion,
-            TCP_TABLE_CLASS tblClass,
-            int reserved);
-
-        public static MIB_TCPROW_OWNER_PID[] GetAllTcpConnections()
-        {
-            MIB_TCPROW_OWNER_PID[] tTable;
-            int AF_INET = 2;    // IP_v4
-            int buffSize = 0;
-
-            // how much memory do we need?
-            uint ret = GetExtendedTcpTable(IntPtr.Zero,
-                ref buffSize,
-                true,
-                AF_INET,
-                TCP_TABLE_CLASS.TCP_TABLE_OWNER_PID_ALL,
-                0);
-            if (ret != 0 && ret != 122) // 122 insufficient buffer size
-                throw new Exception("bad ret on check " + ret);
-            IntPtr buffTable = Marshal.AllocHGlobal(buffSize);
-
-            try
-            {
-                ret = GetExtendedTcpTable(buffTable,
-                    ref buffSize,
-                    true,
-                    AF_INET,
-                    TCP_TABLE_CLASS.TCP_TABLE_OWNER_PID_ALL,
-                    0);
-                if (ret != 0)
-                    throw new Exception("bad ret " + ret);
-                // get the number of entries in the table
-                MIB_TCPTABLE_OWNER_PID tab =
-                    (MIB_TCPTABLE_OWNER_PID)Marshal.PtrToStructure(buffTable, typeof(MIB_TCPTABLE_OWNER_PID));
-                IntPtr rowPtr = (IntPtr)((long)buffTable +
-                    Marshal.SizeOf(tab.dwNumEntries));
-                tTable = new MIB_TCPROW_OWNER_PID[tab.dwNumEntries];
-
-                for (int i = 0; i < tab.dwNumEntries; i++)
-                {
-                    MIB_TCPROW_OWNER_PID tcpRow = (MIB_TCPROW_OWNER_PID)Marshal.PtrToStructure(rowPtr, typeof(MIB_TCPROW_OWNER_PID));
-                    tTable[i] = tcpRow;
-                    // next entry
-                    rowPtr = (IntPtr)((long)rowPtr + Marshal.SizeOf(tcpRow));
-                }
-            }
-            finally
-            {
-                // Free the Memory
-                Marshal.FreeHGlobal(buffTable);
-            }
-            return tTable;
-        }
-
-        // Try and get the process.
-        public static bool TryGetProcess(string processName, out Process process)
-        {
-            var proccesses = Process.GetProcessesByName(processName);
-
-            if (proccesses.Any())
-                process = proccesses[0];
-            else
-                process = default;
-
-            // Had to downgrade to .net 7 lol.
-            return process != null;
-        }
-
-        // Get the local address of Terraria dynamically.
+        // Get the local address of the process dynamically.
         private void button1_Click(object sender, EventArgs e)
         {
-            TryGetProcess(textBox1.Text, out var tProccess);
-
-            if (tProccess == null)
+            if (TcpConnectionInfo.TryGetLocalPort(textBox1.Text, out ushort outPort) != 0)
+            {
+                MessageBox.Show("ERROR: The " + outPort + " proccess was not found.");
+            }
+            else
             {
                 MessageBox.Show("ERROR: The " + textBox1.Text + " proccess was not found.");
-                return;
-            }
-
-            // Get each process.
-            foreach (var process in GetAllTcpConnections())
-            {
-                // Check if the process PID is the same as the games PID.
-                if (process.owningPid == tProccess.Id)
-                {
-                    MessageBox.Show(textBox1.Text + "'s Local Port Is: " + process.LocalPort.ToString());
-                    break;
-                }
             }
         }
     }
