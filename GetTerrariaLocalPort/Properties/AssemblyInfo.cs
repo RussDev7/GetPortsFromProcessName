235bd943fa4a094859b4aa7bc4a2b2c515918ac9
DannyRuss <33048298+RussDev7@users.noreply.github.com>
2022-11-21 19:51:31 -0800

    Update AssemblyInfo.cs

--- GetTerrariaLocalPort/Properties/AssemblyInfo.cs
+++ GetTerrariaLocalPort/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GetTerrariaLocalPort")]
+[assembly: AssemblyTitle("GetPortsFromProcessName")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GetTerrariaLocalPort")]
+[assembly: AssemblyProduct("GetPortsFromProcessName")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
